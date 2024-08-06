5b9145c159de963998df5134b09afa4dabe3a548
Md Kamran Taslim <54260472+KamranTaslim@users.noreply.github.com>
2024-08-06 16:53:32 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,16 +1,15 @@
 const socket = io();
-
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
+const toggleMicButton = document.getElementById("toggleMic");
+const toggleVideoButton = document.getElementById("toggleVideo");
 
 const peerConnectionConfig = {
   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
 };
-
 let localStream;
 let peerConnection;
 
-// Get user media (audio and video)
 navigator.mediaDevices
   .getUserMedia({ video: true, audio: true })
   .then((stream) => {
@@ -22,21 +21,13 @@ navigator.mediaDevices
 
 function initializePeerConnection() {
   peerConnection = new RTCPeerConnection(peerConnectionConfig);
-
-  // Add local stream tracks to the connection
   localStream
     .getTracks()
     .forEach((track) => peerConnection.addTrack(track, localStream));
-
-  // Handle incoming tracks
-  peerConnection.ontrack = (event) => {
-    remoteVideo.srcObject = event.streams[0];
-  };
-
+  peerConnection.ontrack = (event) =>
+    (remoteVideo.srcObject = event.streams[0]);
   peerConnection.onicecandidate = (event) => {
-    if (event.candidate) {
-      socket.emit("ice-candidate", event.candidate);
-    }
+    if (event.candidate) socket.emit("ice-candidate", event.candidate);
   };
 
   socket.on("offer", async (offer) => {
@@ -56,13 +47,27 @@ function initializePeerConnection() {
     peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
   });
 
-  // Create and send an offer
-  async function createOffer() {
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-    socket.emit("offer", offer);
-  }
-
-  // Uncomment the following line to create an offer
   createOffer();
 }
+
+async function createOffer() {
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  socket.emit("offer", offer);
+}
+
+toggleMicButton.addEventListener("click", () => {
+  localStream.getAudioTracks()[0].enabled =
+    !localStream.getAudioTracks()[0].enabled;
+  toggleMicButton.textContent = localStream.getAudioTracks()[0].enabled
+    ? "Mute Mic"
+    : "Unmute Mic";
+});
+
+toggleVideoButton.addEventListener("click", () => {
+  localStream.getVideoTracks()[0].enabled =
+    !localStream.getVideoTracks()[0].enabled;
+  toggleVideoButton.textContent = localStream.getVideoTracks()[0].enabled
+    ? "Stop Video"
+    : "Start Video";
+});
